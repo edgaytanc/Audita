be68d9c802348e860317c3f684c250f94058827c
David Gaytan <edgaytanc@gmail.com>
2023-03-11 23:31:14 -0600

    se modifico la apliación eliminando la app login y se utiliza auth de django para el manejo de login, este carga al inicio

--- core/settings.py
+++ core/settings.py
@@ -37,12 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login',
-    #django tweaks
     'widget_tweaks',
-    
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -130,3 +130,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+
--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('login.urls')),
-    path('', views.index, name="index"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', auth_views.LoginView.as_view(), name='login'),
+    
 ]
